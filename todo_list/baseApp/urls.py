4e530f911b4047aeedcd513e8bfb64eb539a409f
Haris Leghari <harisleghari61@gmail.com>
2022-10-11 12:49:14 +0500

    Delete Page

--- todo_list/baseApp/urls.py
+++ todo_list/baseApp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import CreateList, DetailList, TaskList, DetailView, UpdateList
+from .views import CreateList, DetailList, TaskList, UpdateList, DeleteList
 
 urlpatterns = [
     path('', TaskList.as_view(), name="tasks"),
     path('task/<int:pk>', DetailList.as_view(), name="detail"),
     path('create-list', CreateList.as_view(), name="create"),
-    path('update/<int:pk>', UpdateList.as_view(), name="update")
+    path('update/<int:pk>', UpdateList.as_view(), name="update"),
+    path('delete/<int:pk>', DeleteList.as_view(), name="delete")
+
 
     
 ]
--- todo_list/baseApp/views.py
+++ todo_list/baseApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
@@ -31,4 +31,11 @@ class UpdateList(UpdateView):
     model = Task
     fields = '__all__'
     template_name = 'base/task_create.html'
+    success_url = reverse_lazy('tasks')
+
+
+class DeleteList(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task_delete.html'
     success_url = reverse_lazy('tasks')
