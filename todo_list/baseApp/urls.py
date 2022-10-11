b0614bfbe63fb0789dd78d227c5014dc8542be86
Haris Leghari <harisleghari61@gmail.com>
2022-10-11 12:35:04 +0500

    Update Page

--- todo_list/baseApp/urls.py
+++ todo_list/baseApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CreateList, DetailList, TaskList, DetailView
+from .views import CreateList, DetailList, TaskList, DetailView, UpdateList
 
 urlpatterns = [
     path('', TaskList.as_view(), name="tasks"),
     path('task/<int:pk>', DetailList.as_view(), name="detail"),
-    path('create-list', CreateList.as_view(), name="create")
+    path('create-list', CreateList.as_view(), name="create"),
+    path('update/<int:pk>', UpdateList.as_view(), name="update")
 
     
 ]
