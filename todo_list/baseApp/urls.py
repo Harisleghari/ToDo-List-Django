d3225311f5647e079a95e43cab6b5f7125a63531
Haris Leghari <harisleghari61@gmail.com>
2022-10-11 11:45:39 +0500

    Detail Page

--- todo_list/baseApp/urls.py
+++ todo_list/baseApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TaskList
+from .views import DetailList, TaskList, DetailView
 
 urlpatterns = [
-    path('', TaskList.as_view(), name="tasks")
+    path('', TaskList.as_view(), name="tasks"),
+    path('task/<int:pk>', DetailList.as_view(), name="detail")
+    
 ]
--- todo_list/baseApp/views.py
+++ todo_list/baseApp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Task
 
 
 class TaskList(ListView):
     model = Task
     template_name = 'base/task_list.html'
+    context_object_name = 'tasks'
+
+
+class DetailList(DetailView):
+    model = Task
+    template_name = 'base/task_detail.html'
     context_object_name = 'tasks'
